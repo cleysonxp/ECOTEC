a21c6b36183679ff0353c855eef7ef8664002eb1
cleyson <cleyson.ribeiro.777@gmail.com>
2026-04-08 13:42:27 -0300

    criação inicial do projeto

---
+++ BackEnd/EcoTec.API/EcoTec.Shared.Dados/Banco/EcoTecContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoTec.Shared.Dados.Banco;
+
+internal class EcoTecContext: DbContext
+{
+    private readonly string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EcoTec;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder
+                .UseSqlServer(_connectionString);
+                //.UseLazyLoadingProxies();
+        }
+    }
+}
---
+++ BackEnd/EcoTec.API/EcoTec.Shared.Modelos/Modelo/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoTec.Shared.Modelos.Modelo;
+
+public class Usuario
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required, MaxLength(100)]
+    public string Nome { get; set; } = string.Empty;
+
+    [Required, MaxLength(100)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, MaxLength(100)]
+    public string Senha { get; set; } = string.Empty;
+}
